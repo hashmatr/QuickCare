f1c4ed4e5153cbe97792aca049ab971b134d3fb1
hashmat <hashmatraza.contact@gmail.com>
2025-08-04 16:31:03 +0500

    default

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-green-200 px-5 py-6'>I am going to make a website in react</h1>
+    </div>
+  )
+}
+
+export default App
