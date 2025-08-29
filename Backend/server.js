b82839d36a3b1909bb48b7467d813b26356300f3
hashmat <hashmatraza.contact@gmail.com>
2025-08-29 15:45:05 +0500

    backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,18 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
 const app = express();
 const port = process.env.PORT|| 4000
+connectDB()
+connectCloudinary()
 app.use(express.json())
 app.use(cors())
 app.get('/',(req,res)=>{
     res.send('APi is Working')
 })
+
 app.listen(port,()=>{
     console.log('Listening.....');
 })
